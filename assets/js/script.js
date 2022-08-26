bc950621cf489fc2b179d299b4bd180df34bac36
connork96 <connorkelly96@yahoo.com>
2022-08-25 23:47:22 -0400

    created a create row function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-    $("search-button").on("click", function() {
+    $("search-btn").on("click", function() {
         var searchValue = $("#search-value").val();
         $("#search-value").val("");
         searchWeather(searchValue);
     })
-})
+});
+
+$(".history").on("click", "li", function(){
+    searchWeather($(this).text());
+});
+
+function createRow(text) {
+    var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+    $(".history").append(li);
+}
+
