ee91317b30f63777027ef0d2e16ef09fc22932ea
connork96 <connorkelly96@yahoo.com>
2022-08-25 23:36:29 -0400

    changed aside to section elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("search-button").on("click", function() {
+        var searchValue = $("#search-value").val();
+        $("#search-value").val("");
+        searchWeather(searchValue);
+    })
+})
